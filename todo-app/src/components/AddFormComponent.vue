e41caf2933ef85d2d61ea862afbbb04b638dfeba
Kevin Octavianus Halim <vainezz121@gmail.com>
2024-04-06 16:36:23 +0700

    add view init

---
+++ todo-app/src/components/AddFormComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-3">
+    <form>
+      <div class="mb-3">
+        <label for="userId" class="form-label">User Id</label>
+        <input type="number" class="form-control" id="userId" aria-describedby="userHelp" />
+        <div id="userHelp" class="form-text">Insert User Id</div>
+      </div>
+      <div class="mb-3">
+        <label for="taskInput" class="form-label">Task Title</label>
+        <input type="text" class="form-control" id="taskInput" />
+        <div id="taskHelp" class="form-text">Insert User's Task</div>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from './atoms/ButtonComponent.vue'
+export default {
+  components: {
+    ButtonComponent
+  }
+}
+</script>
---
+++ todo-app/src/views/AddTaskView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container my-2">
+    <h1 class="text-center">Add New Task</h1>
+    <AddFormComponent />
+  </div>
+</template>
+
+<script>
+import AddFormComponent from '@/components/AddFormComponent.vue'
+export default {
+  components: {
+    AddFormComponent
+  }
+}
+</script>
